a7b11a14ba788acc152a238f45a7009401c78290
bmellish <benjamin.p.mellish@gmail.com>
2016-09-24 16:36:12 -0700

    Update open_mp_functions.hpp

--- open_mp_functions/open_mp_functions.hpp
+++ open_mp_functions/open_mp_functions.hpp
@@ -14,15 +14,26 @@
 class omp_tester
 {
 private:
+    // This will add a one nanoseconds sleep to the normal_sine function.
+    // Turn this on if you want to simulate the overhead of kicking off a thread.
     bool add_sleep = false;
+    // A simple struct for holding the mean and standard deviation returned from
+    // the get_stats function.
     struct Stats {
         double mean;
         double std_dev;
     };
+    // Run a simple sin function in a for loop with an omp decoration.
     void omp_sine();
+    // Run the same simple sin function with straight line code.
     void normal_sine();
+    // Run a bunch of long running tasks (many calls to the function that 
+    // generates the sin array) sequentially.
     void run_sequential_tasks();
+    // Run a bunch of long running tasks in parallel.
     void run_omp_tasks();
+    // Calculate the mean and the standard deviation of a vector of ints.
+    // TODO: This would be a good place to show template usage.
     Stats get_stats(std::vector<int>);
 public:
 
